538e0cf1e1433b83bcd0761b817950b767af9e36
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-05-12 11:17:28 +0000

    Add findpy.py that finds all Python code files

---
+++ findpy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import fnmatch
+import magic
+
+def check_py(fname):
+    return magic.from_file(fname, mime=True) == 'text/x-python'
+
+def Walk(root='.', recurse=True, pattern='*'):
+    """
+        Generator for walking a directory tree.
+        Starts at specified root folder, returning files
+        that match our pattern. Optionally will also
+        recurse through sub-folders.
+    """
+    for path, subdirs, files in os.walk(root):
+        dirname = os.path.basename(path)
+        if dirname in ('env', '.git') \
+                or dirname.endswith('.egg-info'):
+            del subdirs[:]
+            continue
+        for name in files:
+            fname = os.path.join(path, name)
+            if fnmatch.fnmatch(name, pattern) or check_py(fname):
+                yield fname
+        if not recurse:
+            break
+
+for fname in Walk('.', True, '*.py'):
+    print fname
